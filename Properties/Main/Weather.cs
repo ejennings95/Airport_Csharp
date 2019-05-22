0161fd40e370b5ffcfdb5f1a82744d6d37973c03
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-05-22 17:15:49 +0100

    :sparkles: weather class randomly picks Clear or Stormy. Test passing.

---
+++ Properties/Main/Weather.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Airport.Csharp
+{
+    public class Weather
+    {
+        public string condition = "Clear"; 
+
+        public Weather()
+        {
+        }
+
+        public string GetCondition()
+        {
+            SetCondition();
+            return condition;
+        }
+
+        public void SetCondition()
+        {
+            if (Random() == 0)
+            {
+                condition = "Clear";
+            }
+            else
+            {
+                condition = "Stormy";
+            }
+        }
+
+        public virtual int Random()
+        {
+            var rnd = new Random().Next(0, 2);
+            return rnd;
+        }
+    }
+}
---
+++ Properties/Test/WeatherTest.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+namespace Airport.Csharp.Test
+{
+    public class WeatherTest
+    {
+        [Test, Description("Weather has default condition of Clear")]
+        public void DefaultCondition()
+        {
+            Weather weather = new Weather();
+            Assert.AreEqual("Clear", weather.condition);
+        }
+
+        [Test, Description("Weather condition is Clear is random returns 0")]
+        public void ClearCondition()
+        {
+            var weather = new Mock<Weather>();
+            weather.Setup(x =>  x.Random()).Returns(0);
+            weather.Object.GetCondition();
+            Assert.AreEqual("Clear", weather.Object.condition);
+        }
+
+        [Test, Description("Weather condition is Stormy is random returns 1")]
+        public void StormyCondition()
+        {
+            var weather = new Mock<Weather>();
+            weather.Setup(x => x.Random()).Returns(1);
+            weather.Object.GetCondition();
+            Assert.AreEqual("Stormy", weather.Object.condition);
+        }
+    }
+}
