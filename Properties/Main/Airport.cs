be7d2a874fb0b11ece3022afdc68417eb3571a93
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-05-23 12:28:52 +0100

    :recycle: refactored Airport class.

--- Properties/Main/Airport.cs
+++ Properties/Main/Airport.cs
@@ -35,40 +35,45 @@ namespace Airport.Csharp
 
         public void LandPlane (Plane plane)
         {
-            if (GetHangar().Count >= GetCapacity())
-            {
-                throw new AirportIsFullException("This airport is full");
-            }
-            if (weatherCondition.GetCondition() == "Stormy")
-            {
-                throw new BadWeatherException("Weather too bad to land");
-            }
-            if (plane.GetStatus() == "Landed")
-            {
-                throw new PlaneAlreadyLandedException("Plane already laneded");
-            } else
-            {
-                hangar.Add(plane);
-            }
+            if (IsAirportFull()) throw new AirportIsFullException("This airport is full");
+            if (IsStormy()) throw new BadWeatherException("Weather too bad to land");
+            if (IsLanded(plane)) throw new PlaneAlreadyLandedException("Plane already landed");
+            hangar.Add(plane);
+            plane.LandedStatus();
         }
 
         public void TakeOffPlane(Plane plane)
         {
-            if (weatherCondition.GetCondition() == "Stormy")
-            {
-                throw new BadWeatherException("Weather too bad to take off");
-            }
-            if (plane.GetStatus() == "Flying")
-            {
-                throw new PlaneAlreadyInFlightException("Plane is already in flight");
-            }
-            if (!GetHangar().Contains(plane)) 
-            {
-                throw new PlaneNotInHangarException("This plane is not in this airport's hangar");
-            } else
-            {
-                hangar.Remove(plane);
-            }
+            if (IsStormy()) throw new BadWeatherException("Weather too bad to take off");
+            if (IsFlying(plane)) throw new PlaneAlreadyInFlightException("Plane is already in flight");
+            if (!IsPlaneInHanagar(plane)) throw new PlaneNotInHangarException("This plane is not in this airport's hangar");
+            hangar.Remove(plane);
+            plane.FlyingStatus();
+        }
+
+        private bool IsStormy()
+        {
+            return weatherCondition.GetCondition() == "Stormy" ? true : false;
+        }
+
+        private bool IsFlying(Plane plane)
+        {
+            return plane.GetStatus() == "Flying" ? true : false;
+        }
+
+        private bool IsLanded(Plane plane)
+        {
+            return plane.GetStatus() == "Landed" ? true : false;
+        }
+
+        private bool IsAirportFull()
+        {
+            return GetHangar().Count >= GetCapacity() ? true : false;
+        }
+
+        private bool IsPlaneInHanagar(Plane plane)
+        {
+            return GetHangar().Contains(plane) ? true : false;
         }
     }
 }
